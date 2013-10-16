a907a41caa7d560b85d48a70135fd220830520b0
zxc2694 <zxc2694zxc2694@yahoo.com.tw>
2013-10-16 22:39:23 +0800

    Delete kill itself and serial stdio

--- emulate.sh
+++ emulate.sh
@@ -8,7 +8,6 @@ emulate () {
 	$QEMU_STM32 \
 		-M stm32-p103 \
 		-kernel $1 \
-		-serial stdio \
 		-parallel none \
 		-monitor tcp:localhost:4444,server,nowait <&0 & pid=$!
 }
@@ -17,14 +16,11 @@ stm32_qemu () {
 	emulate $1
 
 	echo "Modeling STM32 in QEMU..."
-	(sleep $2; kill $pid; sleep 1; kill -KILL $pid)& timer=$!
 	if ! wait $pid; then
-		kill $timer 2>/dev/null
 		echo
 		echo "Modeling failed to execute in $2 seconds, giving up."
 		exit -1
 	fi
-	kill $timer
 }
 
 stm32_qemu $1 5
